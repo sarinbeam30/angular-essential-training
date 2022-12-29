3cffa9cb5fdfc1d5cc73c3fd1b2d72a679916a57
Sarin Wanichwasin <s.wanichwasin_rft@thomsonreuters.com>
2022-12-29 15:22:36 +0700

    add a category-list pipe

---
+++ src/app/category-list.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: 'categoryList'
+})
+
+export class CategoryListPipe implements PipeTransform {
+  transform(mediaItems) {
+    const categories = [];
+    mediaItems.forEach(mediaItem => {
+      console.log('[category-list.pipe.ts] debug-1 : ' , categories.indexOf(mediaItem.category));
+      if (categories.indexOf(mediaItem.category) <= -1) {
+        console.log('[category-list.pipe.ts] debug-2 : ' , mediaItem.category);
+        categories.push(mediaItem.category);
+      }
+    });
+
+    console.log('[category-list.pipe.ts] debug-3 : ' ,categories.join(', '));
+    return categories.join(', ');
+  }
+}
